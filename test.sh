5b3657a34ab849721a49d339b3533f626e10a129
Olessya <olesyawixy@gmail.com>
2018-11-15 16:35:38 -0500

    dir up from travis root to run the .sh file was set

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-cd $TRAVIS_BUILD_DIR/
+#cd $TRAVIS_BUILD_DIR/
+cd ..
 "openSpace\tests\Godot_v3.0.6-stable_win64.exe" --path "openSpace" -d -s --path  "openSpace/addons/gut/gut_cmdln.gd" &>> openSpace/tests/logs/result.txt
