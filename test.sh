1e2596bca25902dd0df90a0f47d6caceb6d0e0c8
Olessya <olesyawixy@gmail.com>
2018-11-15 16:28:55 -0500

    test.sh travis directory set

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-"tests\Godot_v3.0.6-stable_win64.exe" --path "openSpace" -d -s --path  "addons/gut/gut_cmdln.gd" &>> tests/logs/result.txt
+#!/bin/sh
+cd $TRAVIS_BUILD_DIR/
+"openSpace\tests\Godot_v3.0.6-stable_win64.exe" --path "openSpace" -d -s --path  "openSpace/addons/gut/gut_cmdln.gd" &>> openSpace/tests/logs/result.txt
