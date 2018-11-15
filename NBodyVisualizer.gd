c083d5be8b397f8a53f7026e89d828ed5ac05130
Rongcui Dong <rongcuid@outlook.com>
2018-11-15 18:59:15 -0500

    Example to add one planet at origin

--- NBodyVisualizer.gd
+++ NBodyVisualizer.gd
@@ -7,32 +7,27 @@ extends Spatial
 var pause = false
 
 var arrayPlanets = []
+
 onready var planet = preload("res://Planet.tscn")
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	
-	var p = planet.instance()
-	add_child(p)
-	p = planet.instance()
-	p.translation = Vector3(1,0,0)
-	add_child(p)
 
-	#$NBodyPhysics.init(2.95912208286e-4)
+	$NBodyPhysics.init(2.95912208286e-4)
 	# This is a test
-	#addPlanet(1, 0,0,0, 0,0,0)
+	addPlanet(1, 0,0,0, 0,0,0)
 
 func _process(delta):
-	pass
+	
 	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-#	if (!pause):
-#		$NBodyPhysics.step(delta)
-		#visualize()
+	if (!pause):
+		$NBodyPhysics.step(delta)
+		updateTranslation()
 
 func addPlanet(m, x, y, z, vx, vy, vz):
 	$NBodyPhysics.addBody(m,x,y,z,vx,vy,vz)
-	arrayPlanets.push_back(load("res://Planet.tscn"))
+	arrayPlanets.push_back(planet.instance())
+	arrayPlanets.back().translation = Vector3(x,y,z)
+	add_child(arrayPlanets.back())
 	
-func visualize():
+func updateTranslation():
 	pass
