3af7420dc7ce6f651bc2688ff10b54a48a8cde1d
Rongcui Dong <rongcuid@outlook.com>
2018-11-15 19:49:02 -0500

    A NBody demo is done with the same solar system as the smoke test

--- NBodyVisualizer.gd
+++ NBodyVisualizer.gd
@@ -4,25 +4,43 @@ extends Spatial
 # var a = 2
 # var b = "textvar"
 
-var pause = false
+var pause = true
+
+var initialized = false
+
+var timeScale = 1.0
 
 var arrayPlanets = []
 
 onready var planet = preload("res://Planet.tscn")
-func _ready():
 
-	$NBodyPhysics.init(2.95912208286e-4)
-	# This is a test
-	addPlanet(1, -1,0,0, 0.1,0,0)
+#func _ready(G):
+#	#$NBodyPhysics.init(2.95912208286e-3)
 
 func _process(delta):
 	
 	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-	if (!pause):
-		$NBodyPhysics.step(delta)
+	if (initialized && !pause):
+		$NBodyPhysics.step(delta * timeScale)
 		updateTranslation()
 
+func init(G):
+	$NBodyPhysics.init(G)
+	initialized = true
+
+func unPause():
+	pause = false
+	
+func pause():
+	pause = true
+	
+func setTimeScale(ts):
+	timeScale = ts
+
+func getTimeScale():
+	return timeScale
+	
 func addPlanet(m, x, y, z, vx, vy, vz):
 	$NBodyPhysics.addBody(m,x,y,z,vx,vy,vz)
 	arrayPlanets.push_back(planet.instance())
