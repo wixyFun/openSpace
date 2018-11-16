6bea93aa0a9ea8914f456f145a1fe7d91da2a9f4
Olessya <olesyawixy@gmail.com>
2018-11-16 13:03:26 -0500

    new unit testing script for godot

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
-#cd $TRAVIS_BUILD_DIR/
+
 cd wixyFun
 "openSpace\tests\Godot_v3.0.6-stable_win64.exe" --path "openSpace" -d -s --path  "openSpace/runtests.gd" &>> openSpace/tests/logs/result.txt
