31e1ee667cc91fb015e7a470825eafbbd047ca6d
Olessya <olesyawixy@gmail.com>
2018-11-15 19:02:38 -0500

    testing changes

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 #cd $TRAVIS_BUILD_DIR/
 #cd ..
 #"openSpace\tests\Godot_v3.0.6-stable_win64.exe" --path "openSpace" -d -s --path  "openSpace/addons/gut/gut_cmdln.gd" &>> openSpace/tests/logs/result.txt
-"openSpace\tests\Godot_v3.0.6-stable_win64.exe"
+"\tests\Godot_v3.0.6-stable_win64.exe"
