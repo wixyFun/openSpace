c7b72b6e3765eb3df27974ccddd58405605d7769
Olessya <olesyawixy@gmail.com>
2018-11-16 16:13:21 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,3 @@
 #!/bin/sh 
 
-# cd wixyFun
-ls
-
-"./tests/Godot_v3.0.6-stable_win64.exe" --path "./" -d -s --path  "./runtests.gd" &>> ./tests/logs/result.txt
-echo "----------------------"
-echo"----------------------"
-cat ./tests/logs/result.txt
+"./tests/Godot_v3.0.6-stable_win64.exe" --path "./" -d -s --path  "./runtests.gd" 
