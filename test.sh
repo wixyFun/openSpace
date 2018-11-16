2930f70b7c0b4288fd93283b37b9e3efc44066f5
Olessya <olesyawixy@gmail.com>
2018-11-16 16:10:22 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,8 @@
 
 # cd wixyFun
 ls
-".\tests\Godot_v3.0.6-stable_win64.exe" --path "openSpace" -d -s --path  "./runtests.gd" &>> ./tests/logs/result.txt
+
+"./tests/Godot_v3.0.6-stable_win64.exe" --path "./" -d -s --path  "./runtests.gd" &>> ./tests/logs/result.txt
+echo "----------------------"
+echo"----------------------"
 cat ./tests/logs/result.txt
