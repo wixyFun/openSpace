f6b5f4578ff6a1deee6dfc3ce7e5890cd41b82fa
Olessya <olesyawixy@gmail.com>
2018-11-17 07:32:23 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 
+wget https://packages.ubuntu.com/en/trusty/scons
 sudo apt-get install build-essential scons pkg-config libx11-dev libxcursor-dev libxinerama-dev \
     libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libfreetype6-dev libssl-dev libudev-dev \
     libxi-dev libxrandr-dev
