944fe149b23700fc834b7f6f45e31e69aa0df35b
Olessya <olesyawixy@gmail.com>
2018-11-17 12:44:32 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,19 +2,19 @@
 
 sudo apt-get update 
 
-sudo apt-get install build-essential 
-sudo apt-get install scons 
-sudo apt-get install pkg-config 
-sudo apt-get install libx11-dev 
-sudo apt-get install libxcursor-dev 
-sudo apt-get install libxinerama-dev 
-sudo apt-get install libgl1-mesa-dev 
-sudo apt-get install libglu-dev 
-sudo apt-get install libasound2-dev 
-sudo apt-get install libpulse-dev 
-sudo apt-get install libfreetype6-dev 
-sudo apt-get install libssl-dev libudev-dev 
-sudo apt-get install libxi-dev libxrandr-dev
+#sudo apt-get install build-essential 
+#sudo apt-get install scons 
+#sudo apt-get install pkg-config 
+#sudo apt-get install libx11-dev 
+#sudo apt-get install libxcursor-dev 
+#sudo apt-get install libxinerama-dev 
+#sudo apt-get install libgl1-mesa-dev 
+#sudo apt-get install libglu-dev 
+#sudo apt-get install libasound2-dev 
+#sudo apt-get install libpulse-dev 
+#sudo apt-get install libfreetype6-dev 
+#sudo apt-get install libssl-dev libudev-dev 
+#sudo apt-get install libxi-dev libxrandr-dev
 
 
 unzip Godot_v3.0.6-stable_linux_headless.64.zip 
