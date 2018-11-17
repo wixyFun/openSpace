809a74a6e9a7bd581dfdc1009c1a8b6508998607
Olessya <olesyawixy@gmail.com>
2018-11-17 09:40:55 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,24 @@
 #!/bin/sh 
 
-sudo apt update
-sudo apt install snapd
-sudo snap install godot
+wget -O build-essential https://packages.ubuntu.com/trusty/build-essential
+wget -O scons https://packages.ubuntu.com/en/trusty/scons
+     -O pkg-config
+wget -O libx11-dev https://packages.ubuntu.com/trusty/libx11-dev
+wget -O libxcursor-dev https://packages.ubuntu.com/trusty/libxcursor-dev
+wget -O libxinerama-dev  https://packages.ubuntu.com/trusty/libxinerama-dev
+wget -O libgl1-mesa-dev  https://packages.ubuntu.com/trusty/libgl1-mesa-dev
+wget -O libglu-dev https://packages.ubuntu.com/trusty/libglu-dev
+wget -O libasound2-dev https://packages.ubuntu.com/trusty/libasound2-dev
+wget -O libpulse-dev https://packages.ubuntu.com/trusty/libpulse-dev
+wget -O libfreetype6-dev https://packages.ubuntu.com/trusty/libfreetype6-dev
+wget -O libssl-dev https://packages.ubuntu.com/trusty/libssl-dev
+wget -O libudev-dev https://packages.ubuntu.com/trusty/admin/libudev-dev
+wget -O libxi-dev https://packages.ubuntu.com/trusty/libxi-dev
+wget -O libxrandr-dev https://packages.ubuntu.com/trusty/libxrandr-dev
 
-echo"++++++++++++++++++++++"
-cd travis/usr/wixyFun/shared
-
-godot -s ./runtests.gd
-
-
-#wget https://packages.ubuntu.com/en/trusty/scons
-#wget https://packages.ubuntu.com/trusty/libx11-dev
-#wget https://packages.ubuntu.com/trusty/libxcursor-dev
-#wget https://packages.ubuntu.com/trusty/libxinerama-dev
-#wget https://packages.ubuntu.com/trusty/libgl1-mesa-dev
-
-#sudo apt-get install build-essential scons pkg-config libx11-dev libxcursor-dev libxinerama-dev \
-   # libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libfreetype6-dev libssl-dev libudev-dev \
-    #libxi-dev libxrandr-dev
+sudo apt-get install build-essential scons pkg-config libx11-dev libxcursor-dev libxinerama-dev \
+   libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libfreetype6-dev libssl-dev libudev-dev \
+   libxi-dev libxrandr-dev
 
 #wget -O dsound.dll https://godotengine.org/download/windows
 #wget https://godotengine.org/download/linux
