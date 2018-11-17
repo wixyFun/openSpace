d16324899c57076a6b2406f22ba7b33d12df4abf
Olessya <olesyawixy@gmail.com>
2018-11-16 20:00:29 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,9 @@
 #wget -O dsound.dll https://godotengine.org/download/windows
 #wget https://godotengine.org/download/windows
 
-"./Godot_v3.0.6-stable_win64.exe" --path "./" -d -s --path  "./runtests.gd" 
+#"./Godot_v3.0.6-stable_win64.exe" --path "./" -d -s --path  "./runtests.gd" 
+sudo apt-get install unzip
+
+unzip godot.zip -d godot
+
+godot -s runtests.gd
