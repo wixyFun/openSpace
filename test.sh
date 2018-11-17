c7679d3733dde56545f378909e5b901eb257ba3c
Olessya <olesyawixy@gmail.com>
2018-11-17 12:28:36 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -17,23 +17,23 @@ sudo apt-get update
 #wget -O libxi-dev https://packages.ubuntu.com/trusty/libxi-dev
 #wget -O libxrandr-dev https://packages.ubuntu.com/trusty/libxrandr-dev
 
-sudo apt-get install build-essential 
-sudo apt-get install scons 
-sudo apt-get install pkg-config 
-sudo apt-get install libx11-dev 
-sudo apt-get install libxcursor-dev 
-sudo apt-get install libxinerama-dev 
-sudo apt-get install libgl1-mesa-dev 
-sudo apt-get install libglu-dev 
-sudo apt-get install libasound2-dev 
-sudo apt-get install libpulse-dev 
-sudo apt-get install libfreetype6-dev 
-sudo apt-get install libssl-dev libudev-dev 
-sudo apt-get install libxi-dev libxrandr-dev
+#sudo apt-get install build-essential 
+#sudo apt-get install scons 
+#sudo apt-get install pkg-config 
+#sudo apt-get install libx11-dev 
+#sudo apt-get install libxcursor-dev 
+#sudo apt-get install libxinerama-dev 
+#sudo apt-get install libgl1-mesa-dev 
+#sudo apt-get install libglu-dev 
+#sudo apt-get install libasound2-dev 
+#sudo apt-get install libpulse-dev 
+#sudo apt-get install libfreetype6-dev 
+#sudo apt-get install libssl-dev libudev-dev 
+#sudo apt-get install libxi-dev libxrandr-dev
 
 #sudo apt-get install p7zip-full
 
-unzip Godot_v3.0.6-stable_x11.64.zip 
+unzip Godot_v3.0.6-stable_linux_headless.64.zip 
 
 ls
 
@@ -47,7 +47,7 @@ sudo chmod -R a+rwx   ./
 #ls -a
 
 
-./Godot_v3.0.6-stable_x11.64 --path ./project.godot -d -s ./runtests.gd
+./Godot_v3.0.6-stable_linux_headless.64 --path ./project.godot -d -s ./runtests.gd
 
 #chmod +x gt
 #chmod +x runtests.gd 
