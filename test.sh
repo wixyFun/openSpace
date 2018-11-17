72f18e20d699460fd6161722a06d4bbe85bb3d70
Olessya <olesyawixy@gmail.com>
2018-11-16 23:14:20 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,11 +5,6 @@
 
 #"./Godot_v3.0.6-stable_win64.exe" --path "./" -d -s --path  "./runtests.gd" 
 
-
-#unzip gt.zip -d gt
-#unzip gt.zip 
-#cd gt
-
 echo "inside-------------------------"
 ls
 #cd ..
