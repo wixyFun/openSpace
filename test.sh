f2f2d7ab58baa92be9863b7b876d2d2944a93cd6
Olessya <olesyawixy@gmail.com>
2018-11-16 19:09:53 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/sh 
 
 #wget -O dsound.dll https://godotengine.org/download/windows
-wget -O dsound.dll https://godotengine.org/download/windows
+wget https://godotengine.org/download/windows
+wget https://directx-11.en.softonic.com/
 ls
 echo "--------------"
 find / -name *.dll*
-echo "--------------"
+echo "-------------"
 "./tests/Godot_v3.0.6-stable_win64.exe" --path "./" -d -s --path  "./runtests.gd" 
