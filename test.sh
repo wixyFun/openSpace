76e38ec42671d6a8546de58bea5c73745fd6049c
Olessya <olesyawixy@gmail.com>
2018-11-16 22:52:22 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,22 +1,22 @@
 #!/bin/sh 
 
 #wget -O dsound.dll https://godotengine.org/download/windows
-#wget https://godotengine.org/download/windows
+#wget https://godotengine.org/download/linux
 
 #"./Godot_v3.0.6-stable_win64.exe" --path "./" -d -s --path  "./runtests.gd" 
 
 
 #unzip gt.zip -d gt
-unzip gt.zip 
-cd gt
+#unzip gt.zip 
+#cd gt
 
 echo "inside---------------------"
 ls
-cd ..
+#cd ..
 
-chmod +x gt
-chmod +x runtests.gd 
+#chmod +x gt
+#chmod +x runtests.gd 
 
-./gt -s ./runtests.gd
+#./gt -s ./runtests.gd
 
 
