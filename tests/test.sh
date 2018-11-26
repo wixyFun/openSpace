fdac60b74f71747817ec92290d74c05f6c013d6b
Rongcui Dong <rongcuid@outlook.com>
2018-11-26 08:39:08 -0500

    Modified tests to properly abort

--- tests/test.sh
+++ tests/test.sh
@@ -1,18 +1,20 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -o pipefail
 
 INCDIR=
-INCDIR+="-I../../../ThirdParty/boost_1_68_0 "
-INCDIR+="-I../../../ThirdParty/eigen "
-INCDIR+="-I.. "
+INCDIR+="-I../thirdparty/boost_1_68_0 "
+INCDIR+="-I../thirdparty/eigen "
+INCDIR+="-I../src/nbody "
 
 echo "Input validation test..."
 g++ -std=c++11 -ggdb ${INCDIR} -w test-inputValidation.cpp -o a.out
-./a.out | tee testinput.log
+./a.out | tee testinput.log || exit 1
 echo "Input validation test passes"
 
 echo "Smoke Test..."
 g++ -std=c++11 -ggdb ${INCDIR} -w smoketest_physics.cpp -o a.out
-./a.out > smoketest.log
+./a.out > smoketest.log || exit 1
 echo "Smoke test passes"
 
 rm -f a.out smoketest.log testinput.log
