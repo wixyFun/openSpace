fdce58fb9ce320f7e66289077c0868b0fd086e47
Rongcui Dong <rongcuid@outlook.com>
2018-11-27 14:44:56 -0500

    Added branch coverage

--- tests/test.sh
+++ tests/test.sh
@@ -19,7 +19,7 @@ g++ -std=c++11 -O0 -ggdb ${INCDIR} -g -fprofile-arcs -ftest-coverage -w test-nbo
 echo "Simulator test passes"
 
 echo "Generating coverage report..."
-gcov test-nbodySimulator -m
+gcov test-nbodySimulator -b -c -m
 echo "=============================="
 cat NBPhysics.hpp.gcov
 echo "Coverage report done"
