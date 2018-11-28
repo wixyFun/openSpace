45acae428622f187473b94d5088fe5fe0c960a2b
Olessya <olesyawixy@gmail.com>
2018-11-28 13:40:55 -0500

    testing

--- tests/menu/runtests.gd
+++ tests/menu/runtests.gd
@@ -1,11 +1,20 @@
 extends SceneTree  
 
-
+"""
 
 func _init():  
     load('res://tests/menu/unittest.gd').run([
         'res://tests/menu/testing_global.gd',
 		'res://tests/menu/testing_menu_grid.gd',
 		'res://tests/menu/testing_main_menu_scene.gd',
+    ])
+    quit()
+	
+"""
+
+func _init():  
+    load('res://tests/menu/unittest.gd').run([
+        'res://tests/menu/testing_sample.gd',
+		
     ])
     quit()
